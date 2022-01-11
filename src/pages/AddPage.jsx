976fffee630e80772540135fc8a824ba38c77ddc
Maliki-debug <fatimazahraemaliki003@gmail.com>
2022-01-11 17:13:42 +0100

    letscommit

--- src/pages/AddPage.jsx
+++ src/pages/AddPage.jsx
@@ -1,13 +1,51 @@
-import React, { useState, useEffect }  from 'react';
+import React from 'react';
+import { useState }  from 'react';
+import { ListItemIcon, IconButton, ListItemText, ListItem, Container, List, Button,Grid, Typography, Select, MenuItem, Drawer } from "@mui/material";
+import { styled, useTheme, makeStyles } from '@material-ui/core/styles';
 
-import { Button, Paper, Stack, TextField,Grid, Typography,RadioGroup, FormControlLabel, FormLabel,Checkbox,FormGroup, Select, MenuItem } from "@mui/material";
-
-import{blue, grey} from "@mui/material/colors";
 import { useNavigate } from "react-router";
-import SaveIcon from '@mui/icons-material/Save';
-import ProgramService from "../services/ProgramService";
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Divider from '@mui/material/Divider';
+import AccessibilityTwoTone from '@mui/icons-material/AccessibilityTwoTone';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+import ProgramService from "../services/ProgramService.js";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 
 export default function AddPage() {
+  const classes = useStyles();
 
   let history = useNavigate();
 
@@ -35,46 +73,224 @@ export default function AddPage() {
                           )
 
   })
+  const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create('margin', {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    }),
+  );
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const theme = useTheme();
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+  const drawerWidth = 300;
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
 
 
     return(
-        <div>
-           <Grid
-                container 
-                justifyContent="center"
-                alignItems="flex-end"
-            >
-          <Paper sx={{p: 2, margin: 2, maxWidth: 500, flexGrow: 1}} elevation={6}>
-          
-            <Stack spacing={1}>    
-                <Typography color={blue[800]} variant='button'>Ajouter un nouveaux étudiant</Typography>
-                <Typography color={grey[500]} variant='body1'>Vous devez remplir tous les champs obligatoires. </Typography>
-                <TextField label="Nom" variant="outlined" helperText="Tappez ici Votre Nom"/>
-                <TextField label="Prenom" variant="outlined" helperText="Tappez ici Votre Prenom"/>
-                <TextField label="Email" variant="outlined" helperText="Tappez ici Votre Email"/>
-                <TextField label="phone" variant="outlined" helperText="Tappez ici Votre téléphone"/>
-                <TextField label="Password" variant="filled" helperText="Tappez ici Votre Password"/>
-                        <Select  label="session" >
+      <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Teach Me
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Gestion des programmes', 'Gestion des sessions', 'Gestion des Formateurs'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <AccessibilityTwoTone /> : <AccessibilityTwoTone />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {[ 'Logout'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <LogoutIcon /> : <LogoutIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+      
+      
+      <Container maxWidth="xs">
 
-                                   {
-                                        sessionData.map(function(item){
-
-                                          return(<MenuItem value={item.id}>{item.title}</MenuItem>);
-
-                                        })
-                                      
-                                    }
-                               
-                        </Select>
-            </Stack> 
-           
-         <Stack sx={{margin: 3}} spacing={2} direction={'row'} justifyContent="center">
-                <Button variant="contained" onClick={gotodashboard} ><SaveIcon/> Enregistrer</Button>
-                
-                 
-         </Stack>
-          </Paper>
+        <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Apprenants
+        </Typography>
+        <form className={classes.form} onSubmit={gotodashboard}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Nom"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="Prénom"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="phone"
+                label="Phone"
+              />
+            </Grid>
+            <FormControl sx={{ m: 2, minWidth: 395 }} variant="outlined" required>
+              <InputLabel htmlFor="outlined-adornment-password">Mot de passe</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={values.password}
+                onChange={handleChange('password')}
+                endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+                }
+                label="Mot de passe"
+              />
+            </FormControl>
+            <FormControl sx={{ m: 2, minWidth: 395 }} required>
+              <InputLabel id="demo-simple-select-label">Session</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"  
+                value={sessionData}
+                autoWidth
+                label="Session" >
+                {
+                  sessionData.map(function(item){
+                    return(<MenuItem value={item.id}>{item.title}</MenuItem>);
+                  })                                
+                }         
+              </Select>
+            </FormControl>
           </Grid>
-        </div>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Create
+          </Button>
+
+        </form>
+      </div>
+    </Container>
+    </Main>
+
+  </Box>
     );
 }
