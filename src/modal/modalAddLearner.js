a5e485142db9653ee6628c1326193c15dd41cc90
youssef radid <radid.yoyo@gmail.com>
2022-01-12 21:14:42 +0100

    addLearner front

--- src/modal/modalAddLearner.js
+++ src/modal/modalAddLearner.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect }  from 'react';
 
-import { Button, Paper, Stack, TextField,Grid, Typography,RadioGroup, FormControlLabel, FormLabel,Checkbox,FormGroup, Select, MenuItem } from "@mui/material";
+import { Button, Paper, Stack, TextField,Grid, IconButton, Typography ,Container, FormControlLabel, FormLabel,Checkbox,FormGroup, Select, MenuItem } from "@mui/material";
 
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
 import{blue, grey} from "@mui/material/colors";
 import SaveIcon from '@mui/icons-material/Save';
 import LearnerService from "../services/LearnerService";
@@ -11,6 +17,28 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import AddIcon from '@mui/icons-material/Add';
+import { styled, useTheme, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 
 export default function AddModel() {
   
@@ -19,6 +47,8 @@ export default function AddModel() {
   const [UserLastname, setUserLastname] = useState("");
   const [UserEmail, setUserEmail] = useState("");
   const [UserPhone, setUserPhone] = useState("");
+
+  const classes = useStyles();
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -41,6 +71,62 @@ export default function AddModel() {
   handleClose();
 
   };
+
+
+  const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create('margin', {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    }),
+  );
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const theme = useTheme();
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+  const drawerWidth = 300;
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
+
 
   return (
     <div>
@@ -86,6 +172,98 @@ export default function AddModel() {
          </Stack>
           </Paper>
           </Grid>
+
+
+          <Container maxWidth="xs">
+
+
+<div className={classes.paper}>
+  <Typography component="h1" variant="h5">
+    Apprenants
+  </Typography>
+  <form className={classes.form}>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          variant="outlined"
+          required
+          fullWidth
+          id="lastName"
+          label="Nom"
+          autoFocus
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          variant="outlined"
+          required
+          fullWidth
+          id="firstName"
+          label="Prénom"
+          onChange={(e) => setUserLastname(e.target.value)}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          variant="outlined"
+          required
+          fullWidth
+          id="email"
+          label="Email"
+          onChange={(e) => setUserEmail(e.target.value)}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          variant="outlined"
+          required
+          fullWidth
+          id="phone"
+          label="Phone"
+          onChange={(e) => setUserPhone(e.target.value)}
+        />
+      </Grid>
+      <FormControl sx={{ m: 2, minWidth: 395 }} variant="outlined" required>
+        <InputLabel htmlFor="outlined-adornment-password">Mot de passe</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.password}
+          onChange={handleChange('password')}
+          endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+            {values.showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+          }
+          label="Mot de passe"
+        />
+      </FormControl>
+
+        </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() => addLearner()}
+          >
+            Create
+          </Button>
+        
+        </form>
+      </div>
+    </Container>
+
+
       </Dialog>
     </div>
   );
